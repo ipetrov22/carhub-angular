439010a17fbfd45e3e4efdd2171e1b943d634b1d
Ivaylo Petrov <ivaylo020@gmail.com>
2020-12-08 23:38:48 +0200

    feat/loading spinner and alerts added when creating an offer

--- src/app/offer/offer.service.ts
+++ src/app/offer/offer.service.ts
@@ -4,15 +4,25 @@ import { AngularFireStorage } from '@angular/fire/storage';
 import { Router } from '@angular/router';
 import { Subscription } from 'rxjs';
 import { finalize } from 'rxjs/operators';
+import { AlertService } from '../core/alert.service';
+import { LoadingService } from '../core/loading.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class OfferService implements OnDestroy {
   sub!: Subscription;
-  constructor(private storage: AngularFireStorage, private db: AngularFirestore, private router: Router) { }
+  constructor(
+    private storage: AngularFireStorage,
+    private db: AngularFirestore,
+    private router: Router,
+    private loadingService: LoadingService,
+    private alertService: AlertService
+  ) { }
 
   create(offer: any): void {
+    this.loadingService.isLoading = true;
+
     const file = offer.image[0];
 
     const filePath = `${Math.pow(Math.random(), 10)}`
@@ -26,10 +36,16 @@ export class OfferService implements OnDestroy {
 
         this.db.collection('offers').add(offer)
           .then(() => {
+            this.loadingService.isLoading = false;
             this.router.navigate(['/']);
+
+            this.alertService.alert.message = 'Offer created!';
+            this.alertService.alert.style = 'alert-success';
           })
           .catch(error => {
-            console.log(error);
+            this.loadingService.isLoading = false;
+            this.alertService.alert.message = error.message;
+            this.alertService.alert.style = 'alert-danger';
           });
       })
     ).subscribe();
@@ -37,5 +53,6 @@ export class OfferService implements OnDestroy {
 
   ngOnDestroy() {
     this.sub.unsubscribe();
+    console.log('unsub');
   }
 }
